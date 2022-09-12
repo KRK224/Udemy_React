8cc7ffb754dd4224885085258039eb85a9653f12
krk224 <hihas93@naver.com>
2022-09-12 21:27:13 +0900

    section29 ch.495까지

--- 07-new-starting-project/src/pages/AllMeetup.js
+++ 07-new-starting-project/src/pages/AllMeetup.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 
 const DUMMY_DATA = [
@@ -22,10 +23,45 @@ const DUMMY_DATA = [
 ];
 
 const AllMeetupPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      'https://react-getting-started-cf200-default-rtdb.firebaseio.com//meetups.json'
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Now Loading...</p>
+      </section>
+    );
+  }
   return (
     <section>
       <h1>All Meetups</h1>
-      <MeetupList meetups={DUMMY_DATA} />
+      <MeetupList meetups={loadedMeetups} />
     </section>
   );
 };
--- 07-new-starting-project/src/pages/NewMeetup.js
+++ 07-new-starting-project/src/pages/NewMeetup.js
@@ -1,13 +1,21 @@
+import { useHistory } from 'react-router-dom';
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 const NewMeetupPage = () => {
+  const history = useHistory();
+
   const addMeetupHandler = (meetupData) => {
-    fetch('firebaseUrl/meetups.json', {
-      method: 'POST',
-      body: JSON.stringify(meetupData),
-      headers: {
-        'Content-Type': 'application/json',
-      },
+    fetch(
+      'https://react-getting-started-cf200-default-rtdb.firebaseio.com//meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    ).then(() => {
+      history.replace('/');
     });
   };
   return (
